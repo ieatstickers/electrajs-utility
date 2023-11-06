baaded2ff334937032c6f2544da41d75034fc6b3
mikeburke <mikeburke.214@gmail.com>
2023-11-06 20:14:23 +0000

    Updated Numbers test to remove loops (to avoid slow tests)

--- src/Numbers/Numbers.test.ts
+++ src/Numbers/Numbers.test.ts
@@ -5,12 +5,9 @@ describe("Numbers", () => {
   describe("random", () => {
     
     it("returns a number within the given range", () => {
-      for (let i = 0; i < 1000; i++)
-      {
-        const randomNum = Numbers.random(50, 100);
-        expect(randomNum).toBeGreaterThanOrEqual(50);
-        expect(randomNum).toBeLessThanOrEqual(100);
-      }
+      const randomNum = Numbers.random(50, 100);
+      expect(randomNum).toBeGreaterThanOrEqual(50);
+      expect(randomNum).toBeLessThanOrEqual(100);
     });
     
     it("returns the correct number when min and max are equal", () => {
@@ -19,21 +16,15 @@ describe("Numbers", () => {
     });
     
     it("handles negative numbers", () => {
-      for (let i = 0; i < 1000; i++)
-      {
-        const randomNum = Numbers.random(-100, -50);
-        expect(randomNum).toBeGreaterThanOrEqual(-100);
-        expect(randomNum).toBeLessThanOrEqual(-50);
-      }
+      const randomNum = Numbers.random(-100, -50);
+      expect(randomNum).toBeGreaterThanOrEqual(-100);
+      expect(randomNum).toBeLessThanOrEqual(-50);
     });
     
     it("handles negative and positive numbers", () => {
-      for (let i = 0; i < 1000; i++)
-      {
-        const randomNum = Numbers.random(-50, 50);
-        expect(randomNum).toBeGreaterThanOrEqual(-50);
-        expect(randomNum).toBeLessThanOrEqual(50);
-      }
+      const randomNum = Numbers.random(-50, 50);
+      expect(randomNum).toBeGreaterThanOrEqual(-50);
+      expect(randomNum).toBeLessThanOrEqual(50);
     });
     
     it("throws an error when min is greater than max", () => {
